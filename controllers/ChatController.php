03f88befcad1b63662d6956eeaf6c0a6fa930b0c
chengm <chengm@mysoft.com.cn>
2016-11-11 17:37:06 +0800

    .

--- controllers/ChatController.php
+++ controllers/ChatController.php
@@ -33,8 +33,9 @@ class ChatController extends Controller
     public function actionIndex()
     {
         $this->layout = 'chat-main';
-        \yii::$app->view->registerCssFile(\yii::getAlias('@web' . '/css/chat.css'), ['depends'=>  'app\assets\AppAsset']);
-        \yii::$app->view->registerJsFile(\yii::getAlias('@web' . '/js/websocket.js'), ['depends'=>  'app\assets\AppAsset']);
+        \yii::$app->view->registerCssFile('/css/chat.css', ['depends'=>  'app\assets\AppAsset']);
+        \yii::$app->view->registerJsFile('/js/websocket.js', ['depends'=>  'app\assets\AppAsset']);
+        
         return $this->render('index');
     }
 
